1a6509c16133f514a36c32cd3c24933b5b3db102
willnet <netwillnet@gmail.com>
2013-11-05 23:24:36 +0900

    Split name data into japanese and english

--- lib/takarabako.rb
+++ lib/takarabako.rb
@@ -2,15 +2,17 @@ require "takarabako/version"
 require 'yaml'
 
 class Takarabako
-  NAMES = YAML.load_file(File.expand_path(File.join('..', 'data', 'names.yml'), __FILE__))
+  NAMES_JA = YAML.load_file(File.expand_path(File.join('..', 'data', 'names.ja.yml'), __FILE__))
+  NAMES_EN = YAML.load_file(File.expand_path(File.join('..', 'data', 'names.en.yml'), __FILE__))
 
   def self.open
     new.to_s
   end
 
   def initialize
-    @head = NAMES['head'].sample
-    @tail = NAMES['tail'].sample
+    names = [NAMES_JA, NAMES_EN].sample
+    @head = names['head'].sample
+    @tail = names['tail'].sample
   end
   
   def to_s
